327bca6834e9c8650e0868f37091faae257dfa2d
“Mason <masonmadaras@gmail.com>
2022-09-20 13:34:01 -0400

    code to test

--- homeworks.js
+++ homeworks.js
@@ -214,3 +214,61 @@
 //   return n.toString().split("").reverse().join('')
 // }
 // console.log(digitize(num3))
+
+// TWO SUM
+// GIVEN AN ARRAY OF INTERGERS (nums) AND AN INTEGER target RETURN indices OF THE two numbers SUCH THAT THEY ADD UP TO target.
+// example 1
+    // input: nums= [2 , 7, 11, 15 ], target = 9
+    // output: [ 0, 1 ]
+    // explantation because nums[0] + nums [1] == 9, we return [ 0,1 ]
+
+// first way to solve it // two loops (nested loops )
+let nums = [ 2, 7, 11, 15 ]
+
+for ( let i =0; i<arr.length; i ++ ){
+    for ( let j= 0; j<arr.length; j++) {
+        if (arr[i] + arr[j] === target) {
+            return [arr[i], arr[j]]
+        }
+    }
+} // TIME COMPLEXITY : O(N^2) // SPACE COMPLEXITY : O(1) CONSTANT
+
+
+// second way to solve it // use a map
+// create a set
+// add every element to the set
+// loop through the array to find if  9-a[i] = x is in the set
+
+let arr= [ 1, 2, 6, 4, 7]
+
+const twoSumswSet= (arr, target) => {
+    const map = new set()
+    for (let i=0; i<arr.length; i ++) {
+        set.add(arr[i]) // THIS ACTS LIKE AN OBJECT WHERE THE KEY AND THE VALUE ARE THE SAME THING
+    }
+    for (let i=0; i < arr.length; i++){
+        const num= target- arr[i]
+        if (set.has(num)){
+            return [arr[i].num]
+        }
+    }
+} // TIME COMPLEXITY : O(N) // SPACE COMPLEXITY: O(N)
+
+// third way of solving it //Sorting the Array 
+// sort the array 
+// use 2 pointer for algorithm to find target
+
+const twoSumSort= (arr, target) => {
+const sort = arr.sort()
+let i = 0, j= sort.length-1
+while (i<j) {
+    const sum = sort[i] + sort[j]
+    if (sum > t) {
+        j --
+    } else if ( sum < t ) {
+        i ++ 
+    } else {
+        return sort[i], sort [j]
+    }
+} // TIME COMPLEXITY: O(NlogN) // SPACE COMPLEXITY: O(N)
+}
